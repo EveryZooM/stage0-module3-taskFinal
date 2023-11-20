6550df5c30160f8201079cce107bce4b6bc04cdb
ZooM <yuhos@mail.ru>
2023-11-21 01:17:32 +0300

    It was quite interesting

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int four = number % 10;
+        int third = (number % 100 - four) / 10;
+        int second = (number % 1000 - (third * 10 + four)) / 100;
+        int first = number / 1000;
+        System.out.println(first + second + third + four);
     }
 }
